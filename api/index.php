6017ecbba93f64d6674fc4d038809de376efe742
faizazza-m <faizfaqot23@gmail.com>
2026-08-14 10:19:07 +0700

    chore: Hapus file konfigurasi Vercel (migrasi ke Railway)

--- api/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$_M = $_SERVER;
-
-$_M['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
-$_M['SCRIPT_NAME'] = '/index.php';
-$_M['PHP_SELF'] = '/index.php';
-
-require __DIR__ . '/../public/index.php';
