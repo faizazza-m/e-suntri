959e3807591485901a0ac061af6b6d021ca18a7c
faizazza-m <faizfaqot23@gmail.com>
2026-08-14 13:31:58 +0700

    fix: Memaksa scheme HTTPS untuk production (Railway) untuk menghindari error form submit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         \Illuminate\Support\Facades\View::composer('layouts.mobile', function ($view) {
             $globalNotifs = collect();
             if (auth()->check() && auth()->user()->role_id == 3) {
